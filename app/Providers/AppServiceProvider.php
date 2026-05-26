ba30f824a628e9b0c0c7a39ee92a236a468c49a2
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-05-25 23:57:08 -0400

    feat: register service and service Contract

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Modules\Auth\Contracts\AuthenticationServiceContract;
+use App\Modules\Auth\Services\AuthenticationService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            AuthenticationServiceContract::class,
+            AuthenticationService::class,
+        );
     }
 
     /**
