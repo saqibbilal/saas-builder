54fc652b2b279511d64e9b8fec51dfa76ca55e27
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-05-25 23:49:05 -0400

    feat: create Login Action to orchestrate login user case

---
+++ app/Modules/Auth/Actions/LoginUserAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Modules\Auth\Actions;
+
+use App\Modules\Auth\Data\LoginData;
+use App\Modules\Auth\Contracts\AuthenticationServiceContract;
+
+class LoginUserAction
+{
+    public function __construct(
+        protected AuthenticationServiceContract $authenticationService,
+    ) {}
+
+    public function __invoke(LoginData $data): array
+    {
+        return $this->authenticationService->login($data);
+    }
+}
