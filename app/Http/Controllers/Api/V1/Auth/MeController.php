565a6adaec2501906118b8ae590e1eda5ab04799
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-05-31 13:49:14 -0400

    feat(auth): add authenticated user endpoint

---
+++ app/Http/Controllers/Api/V1/Auth/MeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+
+class MeController extends Controller
+{
+    public function __invoke(Request $request): UserResource
+    {
+        return new UserResource(
+            $request->user()
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\LoginController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\Auth\MeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')
+    ->get('/v1/auth/me', MeController::class);
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
