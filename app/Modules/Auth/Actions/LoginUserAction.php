917c70c856459a212a8af6653d2e366d05dff722
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-06-01 23:40:46 -0400

    chore: add dispatch for the UserLoggedIn and UserLoggedOut events

--- app/Modules/Auth/Actions/LoginUserAction.php
+++ app/Modules/Auth/Actions/LoginUserAction.php
@@ -4,6 +4,7 @@ namespace App\Modules\Auth\Actions;
 
 use App\Modules\Auth\Contracts\AuthenticationServiceContract;
 use App\Modules\Auth\Data\LoginData;
+use App\Modules\Auth\Events\UserLoggedIn;
 
 class LoginUserAction
 {
@@ -13,6 +14,12 @@ class LoginUserAction
 
     public function __invoke(LoginData $data): array
     {
-        return $this->authenticationService->login($data);
+        $result = $this->authenticationService->login($data);
+
+        UserLoggedIn::dispatch(
+            $result['user']
+        );
+
+        return $result;
     }
 }
--- app/Modules/Auth/Actions/LogoutUserAction.php
+++ app/Modules/Auth/Actions/LogoutUserAction.php
@@ -3,11 +3,14 @@
 namespace App\Modules\Auth\Actions;
 
 use App\Models\User;
+use App\Modules\Auth\Events\UserLoggedOut;
 
 class LogoutUserAction
 {
     public function __invoke(User $user): void
     {
         $user->currentAccessToken()?->delete();
+
+        UserLoggedOut::dispatch($user);
     }
 }
